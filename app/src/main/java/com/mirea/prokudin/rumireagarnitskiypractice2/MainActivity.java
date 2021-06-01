cfa7f02c05170fcf2fb93d376869b8c058bd4d7f
ProkudinVladimir <vova.prokudin.2019@mail.ru>
2021-06-02 00:29:31 +0300

    Practica2

---
+++ app/src/main/java/com/mirea/prokudin/rumireagarnitskiypractice2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mirea.prokudin.rumireaprokudinpractice2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
